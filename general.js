ffb6e1b2e52cb15f73c947f3e32d235f2c3e6ef5
va9iff <va9iff@gmail.com>
2021-07-30 01:00:34 +0400

    wipe the project

--- general.js
+++
@@ -1,7 +0,0 @@
-var el = (elementtype = "div", elementclass = null) => {
-  let element = document.createElement(elementtype);
-  if (elementclass != null) {
-    element.classList.add(elementclass);
-  }
-  return element;
-};
