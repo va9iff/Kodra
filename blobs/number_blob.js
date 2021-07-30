f5e45e30f15d5bf274c865b38af90a864e3344fe
va9iff <va9iff@gmail.com>
2021-07-31 03:42:14 +0400

    switching TypeScript

--- blobs/number_blob.js
+++ blobs/number_blob.js
@@ -3,6 +3,7 @@ import { el } from "./../generals.js";
 import { number_plugin } from "./../plugins/number_plugin.js";
 
 class number_blob extends blob {
+	name = 'number_blob'
 	constructor() {
 		super()
 		this.clear()
